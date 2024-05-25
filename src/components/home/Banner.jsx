382827dff8cef22bcc3372f4fb759f6b8386d178
unknown <you@example.com>
2024-05-25 19:45:53 +0100

    dashborad

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -3,12 +3,12 @@
 function Banner() {
   return (
     <>
-    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://www.freepik.com/free-psd/black-friday-super-sale-facebook-cover-template_34012978.htm#fromView=search&page=1&position=30&uuid=5003af0d-583c-4e7c-a1fb-f6bf73e6dec7)'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       <button className="btn btn-primary">Get Started</button>
     </div>
   </div>
